e9eabad22453c20f75d45f6eca2c6b8e8957705b
Luiz Felipe de Sousa Faria <59565785+lutilipe@users.noreply.github.com>
2020-06-07 09:51:50 -0300

    Update knexfile.ts

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -5,7 +5,7 @@ module.exports = {
     connection: {
         host : '127.0.0.1',
         user : 'root',
-        password : 'Panda2000@3',
+        password : '**********',
         database : 'nlw'
     },
     migrations: {
